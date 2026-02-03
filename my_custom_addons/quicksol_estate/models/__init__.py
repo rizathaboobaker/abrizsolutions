5f38b368eb3a1af2ed170130640bf34da97417f9
rizathaboobaker <hello@abrizae.com>
2026-02-03 17:25:39 +0400

    Delete my_custom_addons/quicksol_estate directory

--- my_custom_addons/quicksol_estate/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from . import amenity
-from . import property
-from . import agent
-from . import lease
-from . import sale
-from . import tenant
--- my_custom_addons/quicksol_estate/models/amenity.py
+++
@@ -1,8 +0,0 @@
-from odoo import models, fields
-
-class Amenity(models.Model):
-    _name = 'real.estate.amenity'
-    _description = 'Property Amenity'
-
-    name = fields.Char('Amenity Name', required=True)
-    description = fields.Text('Description')
